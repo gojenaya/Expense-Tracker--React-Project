53e93c22424fd6ee20a4d9b5fd8169504108fb41
gojenaya <gojenaya@gmail.com>
2023-09-05 12:32:35 -0400

    CSS for Expense Item

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,13 @@
+import './ExpenseItem.css'
+
 function ExpenseItem(){
     return (
-        <div>
-
+        <div className = "expense-item">
+            <div>March 19th 2023</div>
+            <div className = "expense-item__description">
+                <h2>Groceries</h2>
+                <div className = "expense-item__price"> $50.49</div>
+            </div>
         </div>
     );
 }
